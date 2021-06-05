e74b57e02690f432005f8eaf71190f7a7f3859bd
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-06-05 21:32:35 +0300

    4-5 Авторизація

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../../middleware/auth') ///тут знаходиться мыдлвейр, який ми передаэмо ще й у запрос
+
+const User = require('../../models/User')
+
 // @route   GET api/auth
 // @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Auth Works' }));
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+        
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server error')
+    }
+});
+
+
 
 module.exports = router;
