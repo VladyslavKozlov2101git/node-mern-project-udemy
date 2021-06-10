c2770794542de5c485f27a5a11c82d63d15c7907
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-06-10 20:46:55 +0300

    5-2. Add Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth')
 
-// @route   GET api/posts
+const Post = require('../../models/Post')
+const User = require('../../models/User')
+const Profile = require('../../models/Profile')
+
+// @route   POST api/posts
 // @desc    Tests users route
-// @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
+// @access  Private
+
+router.post('/',[auth,[
+    check('text', 'Text is required')
+        .not()
+        .isEmpty()
+
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        })
+
+        const post = await newPost.save()
+
+        res.json(post)
+
+        
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
+
+    
+}
+
+);
 
 module.exports = router;
