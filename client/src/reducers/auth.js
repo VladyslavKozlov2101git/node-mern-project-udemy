b441f2a03583a1a8a9b89174bb0a4390b6176a61
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-06-29 20:46:41 +0300

    9-1. Protected Route For Dashboard

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,13 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
+
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -7,27 +16,38 @@ const initialState = {
   user: null
 };
 
-export default function(state = initialState, action) {
+function authReducer(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
     case REGISTER_SUCCESS:
-      localStorage.setItem('token', payload.token);
+    case LOGIN_SUCCESS:
       return {
         ...state,
         ...payload,
         isAuthenticated: true,
         loading: false
       };
-    case REGISTER_FAIL:
-      localStorage.removeItem('token');
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT:
       return {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: false,
+        user: null
       };
     default:
       return state;
   }
 }
+
+export default authReducer;
